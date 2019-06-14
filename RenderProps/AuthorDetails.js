ba9209d18390f793728258ec11cad1ed68908801
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-06-14 13:12:21 +0800

    Create AuthorDetails.js

---
+++ RenderProps/AuthorDetails.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import Error from "./Error";
+import Loading from "./Loading";
+
+const AuthorDetails = props => {
+    console.log("Auth ",props);
+    const checkLoad = () => {
+        if(props.isLoading)
+            return (<Loading />)
+        else if(props.isError)
+            return (<Error errMsg={props.error} />)
+        return (<Fragment>
+            {props.authors.map((a,i) => {
+                return (<div key={i}>{a}</div>)
+            })}
+        </Fragment>)
+    }
+
+    return (<div>
+        Author Details
+        {checkLoad()}
+    </div>)
+    
+}
+
+export default AuthorDetails;
