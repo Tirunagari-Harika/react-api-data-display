70451ff02c5b7433fb0b3f6421edfa6fd437a486
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-06-14 12:53:32 +0800

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+/* import DataFetcher from "./FetchApi/DataFetcher"; */
+/* import DataFetcher from "./AxiosFetch/DataFetcher"; */
+/* import DataFetcher from "./Async/DataFetcher"; */
+/* import DataFetcher from "./HOC/DataFetcher"; */
+
+import Parent from "./RenderProps/Parent";
+/* import DataFetcher from "./IndiContexts/DataFetcher"; */
+/* import DataFetcher from "./Context/DataFetcher"; */
+
+function Main(){
+    return (<div>
+        <Parent />
+    </div>)
+}
+
+export default Main;
