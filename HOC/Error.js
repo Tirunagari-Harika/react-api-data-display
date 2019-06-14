9e7efef7fe3e0e4f0de22e2b1d411664c892c158
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-06-14 13:07:00 +0800

    Create Error.js

---
+++ HOC/Error.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const Error = props => {
+    console.log("Error ",props);
+    return (<Fragment>
+        Error ...
+    </Fragment>)
+}
+
+export default Error;
