a0c9be0d66b58dece10a1f41ef605db4defa9e9d
Harika Tirunagari <36506029+Tirunagari-Harika@users.noreply.github.com>
2019-06-14 13:04:56 +0800

    Create Error.js

---
+++ FetchApi/Error.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Error = (props) => {
+    console.log(props.errMsg);
+    return (<Fragment>
+        Error ...
+       
+    </Fragment>)
+}
+
+export default Error;
